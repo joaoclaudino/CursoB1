3a050c520915f40ef4a62719ed235991bf840a44
Administrador <Administrador@WIN-3ILJ2PUQN5R>
2020-11-11 11:51:50 -0300

    Trabalhando Com Menus e XML

---
+++ TrabalhandoComMenusEXML/Menus_E_XML.cs
@@ -0,0 +1,42 @@
+﻿using HelperB1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhandoComMenusEXML
+{
+    public class Menus_E_XML
+    {
+        private SAPbouiCOM.Application oApplication;
+
+        public Menus_E_XML()
+        {
+            AppHelper.SetApplication(ref oApplication);
+
+            string strTemp = "MeusMenus.xml";
+            this.LoadFromXML(strTemp);
+        }
+        private void LoadFromXML(string FileName)
+        {
+            System.Xml.XmlDocument oXmlDoc = null;
+            oXmlDoc = new System.Xml.XmlDocument();
+
+            string sPath = null;
+            sPath = Application.StartupPath;
+
+            oXmlDoc.Load(sPath + @"\" + FileName);
+
+            string tmpStr;
+
+            tmpStr = oXmlDoc.InnerXml;
+
+            oApplication.LoadBatchActions(ref tmpStr);
+
+            sPath = oApplication.GetLastBatchResults();
+
+        }
+    }
+}
---
+++ TrabalhandoComMenusEXML/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhandoComMenusEXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menus_E_XML oMenus_E_XML = new Menus_E_XML();
+            System.Windows.Forms.Application.Run();
+        }
+    }
+}
